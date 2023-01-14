8dcf8a5b859bf60d489894adb219ab047d54b5ca
jeffdonkey <8jefa8@gmail.com>
2023-01-14 12:31:25 -0500

    started

--- solution1.py
+++ solution1.py
@@ -1,1 +1,31 @@
 # Write code for algorithm 1 below
+
+def count_down(n):
+
+  #  Base case
+  if n==0:
+      return
+
+  #  Recursive case
+  else:
+      print(n)
+      count_down(n-1)
+
+# test case
+n=8
+count_down(n)
+
+#  OR THIS WORKS ALSO
+
+def count_down(n):
+  #inherent base case
+  #(will stop if n <= 0)
+  if n>0:
+      #recursive case
+      print(n)
+      count_down(n-1)
+   
+#test case
+n=8
+count_down(n)
+
--- solution2.py
+++ solution2.py
@@ -1,1 +1,20 @@
 # Write code for algorithm 2 below
+
+
+def natural_numbers(lowerNum, higherNum):
+    if lowerNum > higherNum:
+        return
+    else:
+        print(lowerNum)
+        natural_numbers(lowerNum + 1, higherNum)
+
+n=10
+natural_numbers(1,n)
+
+# Line 11 sets "n" equal to "10"
+# Line 12 calls function 'natural_numbers' sending '1' and 'n' as arguments
+# Lines 5-6 are the base case.  
+# Lines 7-9 are the recursion case.
+# Line 8 will print the value of 'lowerNum'.
+# Line 9 calls the function "natural_numbers" again; sending 'lowerNum + 1' and the original value for 'higherNum'
+# function repeats until 'lowerNum' value is greater than 10
